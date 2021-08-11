ff785bb1494b53744325e7050002e2b23cb124e4
joshvanl <vleeuwenjoshua@gmail.com>
2021-08-12 00:17:41 +0100

    Move logic from /cmd/app to /internal/cmd
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -17,13 +17,17 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
+	"context"
+	"os"
 
+	"github.com/spf13/cobra"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
 
-	cmpapi "github.com/cert-manager/policy-approver/pkg/apis/policy/v1alpha1"
-	"github.com/cert-manager/policy-approver/pkg/internal/cmd/options"
-	"github.com/cert-manager/policy-approver/pkg/internal/controller"
+	"github.com/cert-manager/policy-approver/apis"
+	"github.com/cert-manager/policy-approver/internal/cmd/options"
+	"github.com/cert-manager/policy-approver/internal/pkg/controller"
+	"github.com/cert-manager/policy-approver/internal/pkg/manager"
 )
 
 const (
@@ -37,41 +41,50 @@ func NewCommand(ctx context.Context) *cobra.Command {
 		Use:   "policy-approver",
 		Short: helpOutput,
 		Long:  helpOutput,
-		PreRunE: func(cmd *cobra.Command, args []string) error {
-			return opts.Complete()
-		},
 		RunE: func(cmd *cobra.Command, args []string) error {
+			opts.Complete()
+			log := opts.Log
+
 			mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-				Scheme:                        cmpapi.GlobalScheme,
+				Scheme:                        apis.Scheme,
+				MetricsBindAddress:            opts.MetricsAddress,
+				HealthProbeBindAddress:        opts.ProbeAddress,
 				LeaderElectionNamespace:       opts.LeaderElectionNamespace,
 				LeaderElection:                true,
 				LeaderElectionID:              "policy.cert-manager.io",
 				LeaderElectionReleaseOnCancel: true,
-				ReadinessEndpointName:         "/readyz",
-				HealthProbeBindAddress:        opts.ReadyzAddress,
-				MetricsBindAddress:            opts.MetricsAddress,
-				Logger:                        opts.Logr.WithName("controller"),
+				Logger:                        log,
 			})
 			if err != nil {
-				return fmt.Errorf("unable to start controller manager: %w", err)
+				log.Error(err, "unable to start manager")
+				os.Exit(1)
 			}
 
-			if err := controller.AddPolicyController(mgr, controller.Options{
-				Log: opts.Logr,
-			}); err != nil {
-				return fmt.Errorf("failed to add policy controller: %w", err)
+			c := controller.New(
+				ctrl.Log, mgr.GetClient(),
+				mgr.GetEventRecorderFor("policy-approver"),
+				manager.New(mgr.GetClient(), opts.ApproveWhenNoPolicies),
+			)
+			if err := c.SetupWithManager(mgr); err != nil {
+				log.Error(err, "unable to create controller", "controller", "CertificateRequestPolicy")
+				os.Exit(1)
 			}
 
+			if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+				log.Error(err, "unable to set up health check")
+				os.Exit(1)
+			}
 			if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
-				return fmt.Errorf("unable to set up ready check: %w", err)
+				log.Error(err, "unable to set up ready check")
+				os.Exit(1)
 			}
 
-			opts.Logr.WithName("main").Info("starting policy controller")
+			log.Info("starting manager")
 			return mgr.Start(ctx)
 		},
 	}
 
-	opts.Prepare(cmd)
+	opts.AddFlags(cmd)
 
 	return cmd
 }
