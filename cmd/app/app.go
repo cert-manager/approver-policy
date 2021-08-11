3215703e054363d02ebe6732a9461180115b83ae
joshvanl <vleeuwenjoshua@gmail.com>
2021-08-12 00:17:41 +0100

    Move logic from /cmd/app to /internal/cmd
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -17,81 +17,18 @@ limitations under the License.
 package app
 
 import (
-	"context"
 	"fmt"
 	"os"
 
-	"github.com/spf13/cobra"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/healthz"
 
-	"github.com/cert-manager/policy-approver/cmd/app/options"
-	"github.com/cert-manager/policy-approver/pkg/api"
-	"github.com/cert-manager/policy-approver/pkg/controllers"
-	"github.com/cert-manager/policy-approver/pkg/policy"
-)
-
-const (
-	helpOutput = "A cert-manager policy approver which bases decisions on CertificateRequestPolicies"
+	"github.com/cert-manager/policy-approver/internal/cmd"
 )
 
 func ExecutePolicyApprover() {
-	cmd := newCommand(ctrl.SetupSignalHandler())
+	cmd := cmd.NewCommand(ctrl.SetupSignalHandler())
 	if err := cmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
-}
-
-func newCommand(ctx context.Context) *cobra.Command {
-	opts := new(options.Options)
-
-	cmd := &cobra.Command{
-		Use:   "policy-approver",
-		Short: helpOutput,
-		Long:  helpOutput,
-		PreRunE: func(cmd *cobra.Command, args []string) error {
-			return opts.Complete()
-		},
-		RunE: func(cmd *cobra.Command, args []string) error {
-
-			mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-				Scheme:                        api.Scheme,
-				LeaderElectionNamespace:       opts.LeaderElectionNamespace,
-				LeaderElection:                true,
-				LeaderElectionID:              "policy.cert-manager.io",
-				LeaderElectionReleaseOnCancel: true,
-				ReadinessEndpointName:         "/readyz",
-				HealthProbeBindAddress:        opts.ReadyzAddress,
-				MetricsBindAddress:            opts.MetricsAddress,
-				Logger:                        opts.Logr.WithName("controller"),
-			})
-			if err != nil {
-				return fmt.Errorf("unable to start controller manager: %w", err)
-			}
-
-			c := controllers.New(
-				ctrl.Log, mgr.GetClient(),
-				mgr.GetEventRecorderFor("policy-approver"),
-				policy.New(mgr.GetClient(), opts.ApproveWhenNoPolicies),
-			)
-			if err := c.SetupWithManager(mgr); err != nil {
-				return fmt.Errorf("unable to create controller CertificateRequestPolicy controller: %w", err)
-			}
-
-			if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
-				return fmt.Errorf("unable to set up health check: %w", err)
-			}
-			if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
-				return fmt.Errorf("unable to set up ready check: %w", err)
-			}
-
-			opts.Logr.WithName("main").Info("starting policy controller")
-			return mgr.Start(ctx)
-		},
-	}
-
-	opts.Prepare(cmd)
-
-	return cmd
 }
---
+++ internal/cmd/cmd.go
@@ -0,0 +1,88 @@
+/*
+Copyright 2021 The cert-manager Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/spf13/cobra"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+
+	"github.com/cert-manager/policy-approver/apis"
+	"github.com/cert-manager/policy-approver/internal/cmd/options"
+	"github.com/cert-manager/policy-approver/internal/pkg/controller"
+	"github.com/cert-manager/policy-approver/internal/pkg/manager"
+)
+
+const (
+	helpOutput = "A cert-manager policy approver which bases decisions on CertificateRequestPolicies"
+)
+
+func NewCommand(ctx context.Context) *cobra.Command {
+	opts := new(options.Options)
+
+	cmd := &cobra.Command{
+		Use:   "policy-approver",
+		Short: helpOutput,
+		Long:  helpOutput,
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			return opts.Complete()
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+				Scheme:                        apis.Scheme,
+				LeaderElectionNamespace:       opts.LeaderElectionNamespace,
+				LeaderElection:                true,
+				LeaderElectionID:              "policy.cert-manager.io",
+				LeaderElectionReleaseOnCancel: true,
+				ReadinessEndpointName:         "/readyz",
+				HealthProbeBindAddress:        opts.ReadyzAddress,
+				MetricsBindAddress:            opts.MetricsAddress,
+				Logger:                        opts.Logr.WithName("controller"),
+			})
+			if err != nil {
+				return fmt.Errorf("unable to start controller manager: %w", err)
+			}
+
+			c := controller.New(
+				ctrl.Log, mgr.GetClient(),
+				mgr.GetEventRecorderFor("policy-approver"),
+				manager.New(mgr.GetClient(), opts.ApproveWhenNoPolicies),
+			)
+			if err := c.SetupWithManager(mgr); err != nil {
+				return fmt.Errorf("unable to create controller CertificateRequestPolicy controller: %w", err)
+			}
+
+			if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+				return fmt.Errorf("unable to set up health check: %w", err)
+			}
+			if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+				return fmt.Errorf("unable to set up ready check: %w", err)
+			}
+
+			opts.Logr.WithName("main").Info("starting policy controller")
+			return mgr.Start(ctx)
+		},
+	}
+
+	opts.Prepare(cmd)
+
+	return cmd
+}
