ad92251f3f146490f5b6d2f193b2e151d9aaf8ac
joshvanl <vleeuwenjoshua@gmail.com>
2021-08-16 16:18:25 +0100

    Adds EvaluatorResponse struct for Evaluate response
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- pkg/approver/approver.go
+++ pkg/approver/approver.go
@@ -16,32 +16,7 @@ limitations under the License.
 
 package approver
 
-import (
-	"context"
-
-	cmapi "github.com/jetstack/cert-manager/pkg/apis/certmanager/v1"
-
-	cmpapi "github.com/cert-manager/policy-approver/pkg/apis/policy/v1alpha1"
-)
-
 // Interface is an Approver. Implements Evaluator.
 type Interface interface {
 	Evaluator
-}
-
-// Evaluator is responsible for making decisions on whether a
-// CertificateRequest should be approved given a CertificateRequestPolicy.
-// Evaluators should register within the register if they wish to be evaluated
-// by the evaluator manager.
-type Evaluator interface {
-	// Evaluate determines whether the given request passes evaluation based on
-	// the given policy.
-	// Evaluate should return "true" if the request is approved, "false"
-	// otherwise.
-	// An occupying message may be returned to give context to give the approval
-	// decision.
-	// An error should only be returned if there was an error in the evaluator
-	// attempting to evaluate the request over the policy itself. A policy
-	// manager may re-evaluate an evaluation if an error is returned.
-	Evaluate(context.Context, *cmpapi.CertificateRequestPolicy, *cmapi.CertificateRequest) (bool, string, error)
 }
