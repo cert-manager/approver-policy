be8ec1ac5981d30135cb222dba5eeb79c127b372
joshvanl <vleeuwenjoshua@gmail.com>
2021-10-11 20:34:08 +0100

    Adds evaluation logic to required allowed approver
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- pkg/approver/allowed/evaluator.go
+++ pkg/approver/allowed/evaluator.go
@@ -56,19 +56,23 @@ func (a Allowed) Evaluate(_ context.Context, policy *policyapi.CertificateReques
 	}
 
 	if len(csr.Subject.CommonName) > 0 {
-		if allowed.CommonName == nil {
-			el = append(el, field.Invalid(fldPath.Child("commonName"), csr.Subject.CommonName, "nil"))
-		} else if !internal.WildcardMatchs(*allowed.CommonName, csr.Subject.CommonName) {
-			el = append(el, field.Invalid(fldPath.Child("commonName"), csr.Subject.CommonName, *allowed.CommonName))
+		if allowed.CommonName == nil || allowed.CommonName.Value == nil {
+			el = append(el, field.Invalid(fldPath.Child("commonName", "value"), csr.Subject.CommonName, "nil"))
+		} else if !internal.WildcardMatchs(*allowed.CommonName.Value, csr.Subject.CommonName) {
+			el = append(el, field.Invalid(fldPath.Child("commonName", "value"), csr.Subject.CommonName, *allowed.CommonName.Value))
 		}
+	} else if allowed.CommonName != nil && allowed.CommonName.Required != nil && *allowed.CommonName.Required {
+		el = append(el, field.Required(fldPath.Child("commonName", "required"), strconv.FormatBool(*allowed.CommonName.Required)))
 	}
 
 	if len(csr.DNSNames) > 0 {
-		if allowed.DNSNames == nil {
-			el = append(el, field.Invalid(fldPath.Child("dnsNames"), csr.DNSNames, "nil"))
-		} else if !internal.WildcardSubset(*allowed.DNSNames, csr.DNSNames) {
-			el = append(el, field.Invalid(fldPath.Child("dnsNames"), csr.DNSNames, strings.Join(*allowed.DNSNames, ", ")))
+		if allowed.DNSNames == nil || allowed.DNSNames.Values == nil {
+			el = append(el, field.Invalid(fldPath.Child("dnsNames", "values"), csr.DNSNames, "nil"))
+		} else if !internal.WildcardSubset(*allowed.DNSNames.Values, csr.DNSNames) {
+			el = append(el, field.Invalid(fldPath.Child("dnsNames", "values"), csr.DNSNames, strings.Join(*allowed.DNSNames.Values, ", ")))
 		}
+	} else if allowed.DNSNames != nil && allowed.DNSNames.Required != nil && *allowed.DNSNames.Required {
+		el = append(el, field.Required(fldPath.Child("dnsNames", "required"), strconv.FormatBool(*allowed.DNSNames.Required)))
 	}
 
 	if len(csr.IPAddresses) > 0 {
@@ -76,11 +80,13 @@ func (a Allowed) Evaluate(_ context.Context, policy *policyapi.CertificateReques
 		for _, ip := range csr.IPAddresses {
 			ips = append(ips, ip.String())
 		}
-		if allowed.IPAddresses == nil {
-			el = append(el, field.Invalid(fldPath.Child("ipAddresses"), ips, "nil"))
-		} else if !internal.WildcardSubset(*allowed.IPAddresses, ips) {
-			el = append(el, field.Invalid(fldPath.Child("ipAddresses"), ips, strings.Join(*allowed.IPAddresses, ", ")))
+		if allowed.IPAddresses == nil || allowed.IPAddresses.Values == nil {
+			el = append(el, field.Invalid(fldPath.Child("ipAddresses", "values"), ips, "nil"))
+		} else if !internal.WildcardSubset(*allowed.IPAddresses.Values, ips) {
+			el = append(el, field.Invalid(fldPath.Child("ipAddresses", "values"), ips, strings.Join(*allowed.IPAddresses.Values, ", ")))
 		}
+	} else if allowed.IPAddresses != nil && allowed.IPAddresses.Required != nil && *allowed.IPAddresses.Required {
+		el = append(el, field.Required(fldPath.Child("ipAddresses", "required"), strconv.FormatBool(*allowed.IPAddresses.Required)))
 	}
 
 	if len(csr.URIs) > 0 {
@@ -88,19 +94,23 @@ func (a Allowed) Evaluate(_ context.Context, policy *policyapi.CertificateReques
 		for _, uri := range csr.URIs {
 			uris = append(uris, uri.String())
 		}
-		if allowed.URIs == nil {
-			el = append(el, field.Invalid(fldPath.Child("uris"), uris, "nil"))
-		} else if !internal.WildcardSubset(*allowed.URIs, uris) {
-			el = append(el, field.Invalid(fldPath.Child("uris"), uris, strings.Join(*allowed.URIs, ", ")))
+		if allowed.URIs == nil || allowed.URIs.Values == nil {
+			el = append(el, field.Invalid(fldPath.Child("uris", "values"), uris, "nil"))
+		} else if !internal.WildcardSubset(*allowed.URIs.Values, uris) {
+			el = append(el, field.Invalid(fldPath.Child("uris", "values"), uris, strings.Join(*allowed.URIs.Values, ", ")))
 		}
+	} else if allowed.URIs != nil && allowed.URIs.Required != nil && *allowed.URIs.Required {
+		el = append(el, field.Required(fldPath.Child("uris", "required"), strconv.FormatBool(*allowed.URIs.Required)))
 	}
 
 	if len(csr.EmailAddresses) > 0 {
-		if allowed.EmailAddresses == nil {
-			el = append(el, field.Invalid(fldPath.Child("emailAddresses"), csr.EmailAddresses, "nil"))
-		} else if !internal.WildcardSubset(*allowed.EmailAddresses, csr.EmailAddresses) {
-			el = append(el, field.Invalid(fldPath.Child("emailAddresses"), csr.EmailAddresses, strings.Join(*allowed.EmailAddresses, ", ")))
+		if allowed.EmailAddresses == nil || allowed.EmailAddresses.Values == nil {
+			el = append(el, field.Invalid(fldPath.Child("emailAddresses", "values"), csr.EmailAddresses, "nil"))
+		} else if !internal.WildcardSubset(*allowed.EmailAddresses.Values, csr.EmailAddresses) {
+			el = append(el, field.Invalid(fldPath.Child("emailAddresses", "values"), csr.EmailAddresses, strings.Join(*allowed.EmailAddresses.Values, ", ")))
 		}
+	} else if allowed.EmailAddresses != nil && allowed.EmailAddresses.Required != nil && *allowed.EmailAddresses.Required {
+		el = append(el, field.Required(fldPath.Child("emailAddresses", "required"), strconv.FormatBool(*allowed.EmailAddresses.Required)))
 	}
 
 	if request.Spec.IsCA {
@@ -133,67 +143,83 @@ func (a Allowed) Evaluate(_ context.Context, policy *policyapi.CertificateReques
 	allowedSub := allowed.Subject
 
 	if len(csr.Subject.Organization) > 0 {
-		if allowedSub == nil || allowedSub.Organizations == nil {
-			el = append(el, field.Invalid(fldPath.Child("organizations"), csr.Subject.Organization, "nil"))
-		} else if !internal.WildcardSubset(*allowedSub.Organizations, csr.Subject.Organization) {
-			el = append(el, field.Invalid(fldPath.Child("organizations"), csr.Subject.Organization, strings.Join(*allowedSub.Organizations, ", ")))
+		if allowedSub == nil || allowedSub.Organizations == nil || allowedSub.Organizations.Values == nil {
+			el = append(el, field.Invalid(fldPath.Child("organizations", "values"), csr.Subject.Organization, "nil"))
+		} else if !internal.WildcardSubset(*allowedSub.Organizations.Values, csr.Subject.Organization) {
+			el = append(el, field.Invalid(fldPath.Child("organizations", "values"), csr.Subject.Organization, strings.Join(*allowedSub.Organizations.Values, ", ")))
 		}
+	} else if allowedSub != nil && allowedSub.Organizations != nil && allowedSub.Organizations.Required != nil && *allowedSub.Organizations.Required {
+		el = append(el, field.Required(fldPath.Child("organizations", "required"), strconv.FormatBool(*allowedSub.Organizations.Required)))
 	}
 
 	if len(csr.Subject.Country) > 0 {
 		if allowedSub == nil || allowedSub.Countries == nil {
-			el = append(el, field.Invalid(fldPath.Child("countries"), csr.Subject.Country, "nil"))
-		} else if !internal.WildcardSubset(*allowedSub.Countries, csr.Subject.Country) {
-			el = append(el, field.Invalid(fldPath.Child("countries"), csr.Subject.Country, strings.Join(*allowedSub.Countries, ", ")))
+			el = append(el, field.Invalid(fldPath.Child("countries", "values"), csr.Subject.Country, "nil"))
+		} else if !internal.WildcardSubset(*allowedSub.Countries.Values, csr.Subject.Country) {
+			el = append(el, field.Invalid(fldPath.Child("countries", "values"), csr.Subject.Country, strings.Join(*allowedSub.Countries.Values, ", ")))
 		}
+	} else if allowedSub != nil && allowedSub.Countries != nil && allowedSub.Countries.Required != nil && *allowedSub.Countries.Required {
+		el = append(el, field.Required(fldPath.Child("countries", "required"), strconv.FormatBool(*allowedSub.Countries.Required)))
 	}
 
 	if len(csr.Subject.OrganizationalUnit) > 0 {
 		if allowedSub == nil || allowedSub.OrganizationalUnits == nil {
-			el = append(el, field.Invalid(fldPath.Child("organizationalUnits"), csr.Subject.OrganizationalUnit, "nil"))
-		} else if !internal.WildcardSubset(*allowedSub.OrganizationalUnits, csr.Subject.OrganizationalUnit) {
-			el = append(el, field.Invalid(fldPath.Child("organizationalUnits"), csr.Subject.OrganizationalUnit, strings.Join(*allowedSub.OrganizationalUnits, ", ")))
+			el = append(el, field.Invalid(fldPath.Child("organizationalUnits", "values"), csr.Subject.OrganizationalUnit, "nil"))
+		} else if !internal.WildcardSubset(*allowedSub.OrganizationalUnits.Values, csr.Subject.OrganizationalUnit) {
+			el = append(el, field.Invalid(fldPath.Child("organizationalUnits", "values"), csr.Subject.OrganizationalUnit, strings.Join(*allowedSub.OrganizationalUnits.Values, ", ")))
 		}
+	} else if allowedSub != nil && allowedSub.OrganizationalUnits != nil && allowedSub.OrganizationalUnits.Required != nil && *allowedSub.OrganizationalUnits.Required {
+		el = append(el, field.Required(fldPath.Child("organizationalUnits", "required"), strconv.FormatBool(*allowedSub.OrganizationalUnits.Required)))
 	}
 
 	if len(csr.Subject.Locality) > 0 {
 		if allowedSub == nil || allowedSub.Localities == nil {
-			el = append(el, field.Invalid(fldPath.Child("localities"), csr.Subject.Locality, "nil"))
-		} else if !internal.WildcardSubset(*allowedSub.Localities, csr.Subject.Locality) {
-			el = append(el, field.Invalid(fldPath.Child("localities"), csr.Subject.Locality, strings.Join(*allowedSub.Localities, ", ")))
+			el = append(el, field.Invalid(fldPath.Child("localities", "values"), csr.Subject.Locality, "nil"))
+		} else if !internal.WildcardSubset(*allowedSub.Localities.Values, csr.Subject.Locality) {
+			el = append(el, field.Invalid(fldPath.Child("localities", "values"), csr.Subject.Locality, strings.Join(*allowedSub.Localities.Values, ", ")))
 		}
+	} else if allowedSub != nil && allowedSub.Localities != nil && allowedSub.Localities.Required != nil && *allowedSub.Localities.Required {
+		el = append(el, field.Required(fldPath.Child("localities", "required"), strconv.FormatBool(*allowedSub.Localities.Required)))
 	}
 
 	if len(csr.Subject.Province) > 0 {
 		if allowedSub == nil || allowedSub.Provinces == nil {
-			el = append(el, field.Invalid(fldPath.Child("provinces"), csr.Subject.Province, "nil"))
-		} else if !internal.WildcardSubset(*allowedSub.Provinces, csr.Subject.Province) {
-			el = append(el, field.Invalid(fldPath.Child("provinces"), csr.Subject.Province, strings.Join(*allowedSub.Provinces, ", ")))
+			el = append(el, field.Invalid(fldPath.Child("provinces", "values"), csr.Subject.Province, "nil"))
+		} else if !internal.WildcardSubset(*allowedSub.Provinces.Values, csr.Subject.Province) {
+			el = append(el, field.Invalid(fldPath.Child("provinces", "values"), csr.Subject.Province, strings.Join(*allowedSub.Provinces.Values, ", ")))
 		}
+	} else if allowedSub != nil && allowedSub.Provinces != nil && allowedSub.Provinces.Required != nil && *allowedSub.Provinces.Required {
+		el = append(el, field.Required(fldPath.Child("provinces", "required"), strconv.FormatBool(*allowedSub.Provinces.Required)))
 	}
 
 	if len(csr.Subject.StreetAddress) > 0 {
 		if allowedSub == nil || allowedSub.StreetAddresses == nil {
-			el = append(el, field.Invalid(fldPath.Child("streetAddresses"), csr.Subject.StreetAddress, "nil"))
-		} else if !internal.WildcardSubset(*allowedSub.StreetAddresses, csr.Subject.StreetAddress) {
-			el = append(el, field.Invalid(fldPath.Child("streetAddresses"), csr.Subject.StreetAddress, strings.Join(*allowedSub.StreetAddresses, ", ")))
+			el = append(el, field.Invalid(fldPath.Child("streetAddresses", "values"), csr.Subject.StreetAddress, "nil"))
+		} else if !internal.WildcardSubset(*allowedSub.StreetAddresses.Values, csr.Subject.StreetAddress) {
+			el = append(el, field.Invalid(fldPath.Child("streetAddresses", "values"), csr.Subject.StreetAddress, strings.Join(*allowedSub.StreetAddresses.Values, ", ")))
 		}
+	} else if allowedSub != nil && allowedSub.StreetAddresses != nil && allowedSub.StreetAddresses.Required != nil && *allowedSub.StreetAddresses.Required {
+		el = append(el, field.Required(fldPath.Child("streetAddresses", "required"), strconv.FormatBool(*allowedSub.StreetAddresses.Required)))
 	}
 
 	if len(csr.Subject.PostalCode) > 0 {
 		if allowedSub == nil || allowedSub.PostalCodes == nil {
-			el = append(el, field.Invalid(fldPath.Child("postalCodes"), csr.Subject.PostalCode, "nil"))
-		} else if !internal.WildcardSubset(*allowedSub.PostalCodes, csr.Subject.PostalCode) {
-			el = append(el, field.Invalid(fldPath.Child("postalCodes"), csr.Subject.PostalCode, strings.Join(*allowedSub.PostalCodes, ", ")))
+			el = append(el, field.Invalid(fldPath.Child("postalCodes", "values"), csr.Subject.PostalCode, "nil"))
+		} else if !internal.WildcardSubset(*allowedSub.PostalCodes.Values, csr.Subject.PostalCode) {
+			el = append(el, field.Invalid(fldPath.Child("postalCodes", "values"), csr.Subject.PostalCode, strings.Join(*allowedSub.PostalCodes.Values, ", ")))
 		}
+	} else if allowedSub != nil && allowedSub.PostalCodes != nil && allowedSub.PostalCodes.Required != nil && *allowedSub.PostalCodes.Required {
+		el = append(el, field.Required(fldPath.Child("postalCodes", "required"), strconv.FormatBool(*allowedSub.PostalCodes.Required)))
 	}
 
 	if len(csr.Subject.SerialNumber) > 0 {
 		if allowedSub == nil || allowedSub.SerialNumber == nil {
-			el = append(el, field.Invalid(fldPath.Child("serialNumber"), csr.Subject.SerialNumber, "nil"))
-		} else if !internal.WildcardMatchs(*allowedSub.SerialNumber, csr.Subject.SerialNumber) {
-			el = append(el, field.Invalid(fldPath.Child("serialNumber"), csr.Subject.SerialNumber, *allowedSub.SerialNumber))
+			el = append(el, field.Invalid(fldPath.Child("serialNumber", "value"), csr.Subject.SerialNumber, "nil"))
+		} else if !internal.WildcardMatchs(*allowedSub.SerialNumber.Value, csr.Subject.SerialNumber) {
+			el = append(el, field.Invalid(fldPath.Child("serialNumber", "value"), csr.Subject.SerialNumber, *allowedSub.SerialNumber.Value))
 		}
+	} else if allowedSub != nil && allowedSub.SerialNumber != nil && allowedSub.SerialNumber.Required != nil && *allowedSub.SerialNumber.Required {
+		el = append(el, field.Required(fldPath.Child("serialNumber", "required"), strconv.FormatBool(*allowedSub.SerialNumber.Required)))
 	}
 
 	// If there are errors, then return not approved and the aggregated errors
