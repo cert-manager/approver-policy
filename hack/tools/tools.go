9f04646fbb120974c99ede748d3b5de891103868
irbekrm <irbekrm@gmail.com>
2023-05-15 18:08:28 +0100

    kustomize v4 -> v5
    This change is needed because latest k8s.io/cli-runtime dep pulls in a version of kustomize api module that is incompatible with kustomize cli
    
    Signed-off-by: irbekrm <irbekrm@gmail.com>

--- hack/tools/tools.go
+++ hack/tools/tools.go
@@ -7,12 +7,11 @@
 package bin
 
 import (
-	_ "sigs.k8s.io/kustomize/kustomize/v4"
-	_ "k8s.io/code-generator/cmd/deepcopy-gen"
 	_ "github.com/norwoodj/helm-docs/cmd/helm-docs"
 	_ "github.com/onsi/ginkgo/v2/ginkgo"
 	_ "github.com/princjef/gomarkdoc/cmd/gomarkdoc"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "sigs.k8s.io/kind"
+	_ "sigs.k8s.io/kustomize/kustomize/v5"
 )
