81d48960de2c80ae528097e94769515e02af8f0a
Erik Godding Boye <egboye@gmail.com>
2024-11-08 21:11:26 +0100

    test(e2e): rework to facilitate adding more tests
    Signed-off-by: Erik Godding Boye <egboye@gmail.com>

--- test/smoke/tests.go
+++ test/smoke/tests.go
@@ -26,7 +26,6 @@ import (
 	cmmeta "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
 	"github.com/cert-manager/cert-manager/test/unit/gen"
 	corev1 "k8s.io/api/core/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -37,60 +36,71 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Smoke", func() {
-	It("should create a CertificateRequestPolicy, RBAC bind to all users, deny and approve a request", func() {
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
+var _ = Describe("Policy", func() {
+	var (
+		ctx       context.Context
+		cl        client.Client
+		namespace corev1.Namespace
+		issuer    cmapi.Issuer
+		policy    policyapi.CertificateRequestPolicy
+	)
 
-		cl, err := client.New(cnf.RestConfig, client.Options{
+	BeforeEach(func() {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithCancel(context.Background())
+
+		var err error
+		cl, err = client.New(cnf.RestConfig, client.Options{
 			Scheme: policyapi.GlobalScheme,
 		})
 		Expect(err).NotTo(HaveOccurred())
 
-		By("Creating test Namespace")
-		namespace := corev1.Namespace{
+		namespace = corev1.Namespace{
 			ObjectMeta: metav1.ObjectMeta{
 				GenerateName: "smoke-test-policy-",
-				Labels:       map[string]string{"foo": "bar"},
 			},
 		}
-		Expect(cl.Create(ctx, &namespace)).NotTo(HaveOccurred())
+		Expect(cl.Create(ctx, &namespace)).To(Succeed())
 
-		By("Creating test SelfSigned Issuer")
-		issuer := cmapi.Issuer{
+		issuer = cmapi.Issuer{
 			ObjectMeta: metav1.ObjectMeta{
 				GenerateName: "smoke-test-policy-",
 				Namespace:    namespace.Name,
 			},
 			Spec: cmapi.IssuerSpec{IssuerConfig: cmapi.IssuerConfig{SelfSigned: &cmapi.SelfSignedIssuer{}}},
 		}
-		Expect(cl.Create(ctx, &issuer)).NotTo(HaveOccurred())
+		Expect(cl.Create(ctx, &issuer)).To(Succeed())
 
-		By("Creating CertificateRequestPolicy for test")
-		policy := policyapi.CertificateRequestPolicy{
+		policy = policyapi.CertificateRequestPolicy{
 			ObjectMeta: metav1.ObjectMeta{
 				GenerateName: "smoke-test-policy-",
 			},
 			Spec: policyapi.CertificateRequestPolicySpec{
-				Allowed: &policyapi.CertificateRequestPolicyAllowed{
-					CommonName: &policyapi.CertificateRequestPolicyAllowedString{Value: ptr.To("*.test.policy")},
-				},
 				Selector: policyapi.CertificateRequestPolicySelector{
 					IssuerRef: &policyapi.CertificateRequestPolicySelectorIssuerRef{
 						Name: ptr.To(issuer.Name),
 					},
 					Namespace: &policyapi.CertificateRequestPolicySelectorNamespace{
-						MatchNames:  []string{"smoke-test-policy-*"},
-						MatchLabels: map[string]string{"foo": "bar"},
+						MatchNames: []string{namespace.Name},
 					},
 				},
 			},
 		}
-		Expect(cl.Create(ctx, &policy)).NotTo(HaveOccurred())
+
+		DeferCleanup(func() {
+			Expect(cl.Delete(ctx, &namespace)).To(Succeed())
+			Expect(cl.Delete(ctx, &policy)).To(Succeed())
+
+			cancel()
+		})
+	})
+
+	JustBeforeEach(func() {
+		Expect(cl.Create(ctx, &policy)).To(Succeed())
 
 		By("Waiting for CertificateRequestPolicy to become ready")
 		Eventually(func() bool {
-			Expect(cl.Get(ctx, client.ObjectKey{Name: policy.Name}, &policy)).NotTo(HaveOccurred())
+			Expect(cl.Get(ctx, client.ObjectKey{Name: policy.Name}, &policy)).To(Succeed())
 			for _, condition := range policy.Status.Conditions {
 				if condition.Type == policyapi.CertificateRequestPolicyConditionReady {
 					return condition.Status == corev1.ConditionTrue && condition.ObservedGeneration == policy.Generation
@@ -98,82 +108,53 @@ var _ = Describe("Smoke", func() {
 			}
 			return false
 		}).WithTimeout(time.Second * 10).WithPolling(time.Millisecond * 10).Should(BeTrue())
+	})
 
-		By("Binding all authenticated users to the test CertificateRequestPolicy")
-		role := rbacv1.Role{
-			ObjectMeta: metav1.ObjectMeta{
-				GenerateName: "smoke-test-policy-",
-				Namespace:    namespace.Name,
-			},
-			Rules: []rbacv1.PolicyRule{
-				{APIGroups: []string{"policy.cert-manager.io"}, Resources: []string{"certificaterequestpolicies"}, ResourceNames: []string{"smoke-test-policy"}, Verbs: []string{"use"}},
-			},
-		}
-		Expect(cl.Create(ctx, &role)).NotTo(HaveOccurred())
+	Context("with allowed CommonName", func() {
+		var certificateRequest cmapi.CertificateRequest
 
-		rolebinding := rbacv1.RoleBinding{
-			ObjectMeta: metav1.ObjectMeta{
-				GenerateName: "smoke-test-policy-",
-				Namespace:    namespace.Name,
-			},
-			RoleRef: rbacv1.RoleRef{APIGroup: "rbac.authorization.k8s.io", Kind: "ClusterRole", Name: role.Name},
-			Subjects: []rbacv1.Subject{
-				{Kind: "Group", Name: "system:authenticated", APIGroup: "rbac.authorization.k8s.io"},
-			},
-		}
-		Expect(cl.Create(ctx, &rolebinding)).NotTo(HaveOccurred())
+		BeforeEach(func() {
+			certificateRequest = cmapi.CertificateRequest{
+				ObjectMeta: metav1.ObjectMeta{
+					GenerateName: "smoke-test-policy-",
+					Namespace:    namespace.Name,
+				},
+				Spec: cmapi.CertificateRequestSpec{
+					IssuerRef: cmmeta.ObjectReference{Name: issuer.Name, Kind: "Issuer", Group: "cert-manager.io"},
+				},
+			}
 
-		By("Creating CertificateRequest that violates policy")
-		csrPEM, _, err := gen.CSR(x509.RSA, gen.SetCSRCommonName("test.foo.policy"))
-		Expect(err).NotTo(HaveOccurred())
+			policy.Spec.Allowed = &policyapi.CertificateRequestPolicyAllowed{
+				CommonName: &policyapi.CertificateRequestPolicyAllowedString{Value: ptr.To("*.test.policy")},
+			}
+		})
 
-		certificateRequest := cmapi.CertificateRequest{
-			ObjectMeta: metav1.ObjectMeta{
-				GenerateName: "smoke-test-policy-",
-				Namespace:    namespace.Name,
-			},
-			Spec: cmapi.CertificateRequestSpec{
-				Request:   csrPEM,
-				IssuerRef: cmmeta.ObjectReference{Name: issuer.Name, Kind: "Issuer", Group: "cert-manager.io"},
-			},
-		}
-		Expect(cl.Create(ctx, &certificateRequest)).NotTo(HaveOccurred())
+		It("should deny request that violates policy", func() {
+			csrPEM, _, err := gen.CSR(x509.RSA, gen.SetCSRCommonName("test.foo.policy"))
+			Expect(err).NotTo(HaveOccurred())
 
-		By("Waiting for CertificateRequest to be denied")
-		Eventually(func() bool {
-			Expect(cl.Get(ctx, client.ObjectKey{Namespace: namespace.Name, Name: certificateRequest.Name}, &certificateRequest)).NotTo(HaveOccurred())
-			return apiutil.CertificateRequestIsDenied(&certificateRequest)
-		}).WithTimeout(time.Second * 10).WithPolling(time.Millisecond * 10).Should(BeTrue())
-		Expect(cl.Delete(ctx, &certificateRequest)).NotTo(HaveOccurred())
+			certificateRequest.Spec.Request = csrPEM
+			Expect(cl.Create(ctx, &certificateRequest)).To(Succeed())
 
-		By("Creating CertificateRequest that passes policy")
-		csrPEM, _, err = gen.CSR(x509.RSA, gen.SetCSRCommonName("foo.test.policy"))
-		Expect(err).NotTo(HaveOccurred())
+			By("Waiting for CertificateRequest to be denied")
+			Eventually(func() bool {
+				Expect(cl.Get(ctx, client.ObjectKey{Namespace: namespace.Name, Name: certificateRequest.Name}, &certificateRequest)).To(Succeed())
+				return apiutil.CertificateRequestIsDenied(&certificateRequest)
+			}).WithTimeout(time.Second * 10).WithPolling(time.Millisecond * 10).Should(BeTrue())
+		})
 
-		certificateRequest = cmapi.CertificateRequest{
-			ObjectMeta: metav1.ObjectMeta{
-				GenerateName: "smoke-test-policy-",
-				Namespace:    namespace.Name,
-			},
-			Spec: cmapi.CertificateRequestSpec{
-				Request:   csrPEM,
-				IssuerRef: cmmeta.ObjectReference{Name: issuer.Name, Kind: "Issuer", Group: "cert-manager.io"},
-			},
-		}
-		Expect(cl.Create(ctx, &certificateRequest)).NotTo(HaveOccurred())
+		It("should approve request that passes policy", func() {
+			csrPEM, _, err := gen.CSR(x509.RSA, gen.SetCSRCommonName("foo.test.policy"))
+			Expect(err).NotTo(HaveOccurred())
 
-		By("Waiting for CertificateRequest to be approved")
-		Eventually(func() bool {
-			Expect(cl.Get(ctx, client.ObjectKey{Namespace: namespace.Name, Name: certificateRequest.Name}, &certificateRequest)).NotTo(HaveOccurred())
-			return apiutil.CertificateRequestIsApproved(&certificateRequest)
-		}).WithTimeout(time.Second * 10).WithPolling(time.Millisecond * 10).Should(BeTrue())
+			certificateRequest.Spec.Request = csrPEM
+			Expect(cl.Create(ctx, &certificateRequest)).To(Succeed())
 
-		By("Cleaning up test resources")
-		Expect(cl.Delete(ctx, &namespace)).NotTo(HaveOccurred())
-		Expect(cl.Delete(ctx, &policy)).NotTo(HaveOccurred())
-		Expect(cl.Delete(ctx, &role)).NotTo(HaveOccurred())
-		Expect(cl.Delete(ctx, &rolebinding)).NotTo(HaveOccurred())
-		Expect(cl.Delete(ctx, &issuer)).NotTo(HaveOccurred())
-		Expect(cl.Delete(ctx, &certificateRequest)).NotTo(HaveOccurred())
+			By("Waiting for CertificateRequest to be approved")
+			Eventually(func() bool {
+				Expect(cl.Get(ctx, client.ObjectKey{Namespace: namespace.Name, Name: certificateRequest.Name}, &certificateRequest)).To(Succeed())
+				return apiutil.CertificateRequestIsApproved(&certificateRequest)
+			}).WithTimeout(time.Second * 10).WithPolling(time.Millisecond * 10).Should(BeTrue())
+		})
 	})
 })
